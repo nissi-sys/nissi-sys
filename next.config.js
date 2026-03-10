ec4991f64305b4271651a6939ffff6d492365e1a
NicolasFrance01 <francenicolas.dw@gmail.com>
2026-03-10 09:38:10 -0300

    debug: add ping route and simplify next config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        serverComponentsExternalPackages: ["@prisma/client", "bcryptjs"]
-    },
+    experimental: {},
     webpack: (config) => {
         config.resolve.alias.canvas = false;
         return config;
