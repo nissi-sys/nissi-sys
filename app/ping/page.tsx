cc0240910eb6781e366f9e2dce7a6a7150b7a69b
NicolasFrance01 <francenicolas.dw@gmail.com>
2026-03-10 09:40:00 -0300

    fix: restore middleware and external packages natively

--- app/ping/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function PingPage() {
-    return <div>pong</div>;
-}
--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {},
+    experimental: {
+        serverComponentsExternalPackages: ["@prisma/client", "bcryptjs"]
+    },
     webpack: (config) => {
         config.resolve.alias.canvas = false;
         return config;
